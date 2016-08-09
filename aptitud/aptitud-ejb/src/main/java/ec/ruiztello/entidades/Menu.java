d872c195c8457de3ee59ffb8109293d38551ae69
christian <christian@localhost>
2016-08-08 21:48:55 -0500

    PRIMERA VENTANA DE MANTENIMIENTO

---
+++ aptitud/aptitud-ejb/src/main/java/ec/ruiztello/entidades/Menu.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.ruiztello.entidades;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author christian
+ */
+@Entity
+@Table(name = "MENU")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Menu.findAll", query = "SELECT m FROM Menu m"),
+    @NamedQuery(name = "Menu.findByIdMenu", query = "SELECT m FROM Menu m WHERE m.idMenu = :idMenu"),
+    @NamedQuery(name = "Menu.findByDescripcionMenu", query = "SELECT m FROM Menu m WHERE m.descripcionMenu = :descripcionMenu")})
+public class Menu implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "ID_MENU")
+    private BigDecimal idMenu;
+    @Size(max = 2000)
+    @Column(name = "DESCRIPCION_MENU")
+    private String descripcionMenu;
+    @JoinColumn(name = "ID_OPCION", referencedColumnName = "ID_OPCIONES")
+    @ManyToOne(optional = false)
+    private Opciones opciones;
+    @JoinColumn(name = "ID_PERFIL", referencedColumnName = "ID_PERFIL")
+    @ManyToOne(optional = false)
+    private Perfil perfil;
+
+    public Menu() {
+    }
+
+    public Menu(BigDecimal idMenu) {
+        this.idMenu = idMenu;
+    }
+
+    public BigDecimal getIdMenu() {
+        return idMenu;
+    }
+
+    public void setIdMenu(BigDecimal idMenu) {
+        this.idMenu = idMenu;
+    }
+
+    public String getDescripcionMenu() {
+        return descripcionMenu;
+    }
+
+    public void setDescripcionMenu(String descripcionMenu) {
+        this.descripcionMenu = descripcionMenu;
+    }
+
+    public Opciones getOpciones() {
+        return opciones;
+    }
+
+    public void setOpciones(Opciones opciones) {
+        this.opciones = opciones;
+    }
+
+    public Perfil getPerfil() {
+        return perfil;
+    }
+
+    public void setPerfil(Perfil perfil) {
+        this.perfil = perfil;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idMenu != null ? idMenu.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Menu)) {
+            return false;
+        }
+        Menu other = (Menu) object;
+        if ((this.idMenu == null && other.idMenu != null) || (this.idMenu != null && !this.idMenu.equals(other.idMenu))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ec.ruiztello.entidades.Menu[ idMenu=" + idMenu + " ]";
+    }
+    
+}
---
+++ aptitud/aptitud-ejb/src/main/java/ec/ruiztello/entidades/Usuario.java
@@ -0,0 +1,283 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.ruiztello.entidades;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author christian
+ */
+@Entity
+@Table(name = "USUARIO")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Usuario.findAll", query = "SELECT u FROM Usuario u"),
+    @NamedQuery(name = "Usuario.findByIdUsuario", query = "SELECT u FROM Usuario u WHERE u.idUsuario = :idUsuario"),
+    @NamedQuery(name = "Usuario.findByDni", query = "SELECT u FROM Usuario u WHERE u.dni = :dni"),
+    @NamedQuery(name = "Usuario.findByNombres", query = "SELECT u FROM Usuario u WHERE u.nombres = :nombres"),
+    @NamedQuery(name = "Usuario.findByApellidos", query = "SELECT u FROM Usuario u WHERE u.apellidos = :apellidos"),
+    @NamedQuery(name = "Usuario.findByClave", query = "SELECT u FROM Usuario u WHERE u.clave = :clave"),
+    @NamedQuery(name = "Usuario.findByEmail", query = "SELECT u FROM Usuario u WHERE u.email = :email"),
+    @NamedQuery(name = "Usuario.findByCelular", query = "SELECT u FROM Usuario u WHERE u.celular = :celular"),
+    @NamedQuery(name = "Usuario.findByConvencional", query = "SELECT u FROM Usuario u WHERE u.convencional = :convencional"),
+    @NamedQuery(name = "Usuario.findByConvencional2", query = "SELECT u FROM Usuario u WHERE u.convencional2 = :convencional2"),
+    @NamedQuery(name = "Usuario.findByFechaCreacion", query = "SELECT u FROM Usuario u WHERE u.fechaCreacion = :fechaCreacion"),
+    @NamedQuery(name = "Usuario.findByFechaModificacion", query = "SELECT u FROM Usuario u WHERE u.fechaModificacion = :fechaModificacion"),
+    @NamedQuery(name = "Usuario.findByEstado", query = "SELECT u FROM Usuario u WHERE u.estado = :estado")})
+public class Usuario implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "ID_USUARIO")
+    private BigDecimal idUsuario;
+    @Size(max = 50)
+    @Column(name = "DNI")
+    private String dni;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "NOMBRES")
+    private String nombres;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 20)
+    @Column(name = "APELLIDOS")
+    private String apellidos;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 100)
+    @Column(name = "CLAVE")
+    private String clave;
+    // @Pattern(regexp="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", message="Invalid email")//if the field contains email address consider using this annotation to enforce field validation
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 150)
+    @Column(name = "EMAIL")
+    private String email;
+    @Size(max = 20)
+    @Column(name = "CELULAR")
+    private String celular;
+    @Size(max = 20)
+    @Column(name = "CONVENCIONAL")
+    private String convencional;
+    @Size(max = 20)
+    @Column(name = "CONVENCIONAL2")
+    private String convencional2;
+    @Column(name = "FECHA_CREACION")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date fechaCreacion;
+    @Column(name = "FECHA_MODIFICACION")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date fechaModificacion;
+    @Size(max = 1)
+    @Column(name = "ESTADO")
+    private String estado;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "usuario")
+    private List<UsuarioXOrganizacion> usuarioXOrganizacionList;
+    @OneToMany(mappedBy = "usuario")
+    private List<Usuario> usuarioList;
+    @JoinColumn(name = "ID_USUARIO_FK", referencedColumnName = "ID_USUARIO")
+    @ManyToOne
+    private Usuario usuario;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "usuario")
+    private List<PerfilXUsuario> perfilXUsuarioList;
+
+    public Usuario() {
+    }
+
+    public Usuario(BigDecimal idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public Usuario(BigDecimal idUsuario, String nombres, String apellidos, String clave, String email) {
+        this.idUsuario = idUsuario;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.clave = clave;
+        this.email = email;
+    }
+
+    public BigDecimal getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(BigDecimal idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getConvencional() {
+        return convencional;
+    }
+
+    public void setConvencional(String convencional) {
+        this.convencional = convencional;
+    }
+
+    public String getConvencional2() {
+        return convencional2;
+    }
+
+    public void setConvencional2(String convencional2) {
+        this.convencional2 = convencional2;
+    }
+
+    public Date getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(Date fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public Date getFechaModificacion() {
+        return fechaModificacion;
+    }
+
+    public void setFechaModificacion(Date fechaModificacion) {
+        this.fechaModificacion = fechaModificacion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    @XmlTransient
+    public List<UsuarioXOrganizacion> getUsuarioXOrganizacionList() {
+        return usuarioXOrganizacionList;
+    }
+
+    public void setUsuarioXOrganizacionList(List<UsuarioXOrganizacion> usuarioXOrganizacionList) {
+        this.usuarioXOrganizacionList = usuarioXOrganizacionList;
+    }
+
+    @XmlTransient
+    public List<Usuario> getUsuarioList() {
+        return usuarioList;
+    }
+
+    public void setUsuarioList(List<Usuario> usuarioList) {
+        this.usuarioList = usuarioList;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    @XmlTransient
+    public List<PerfilXUsuario> getPerfilXUsuarioList() {
+        return perfilXUsuarioList;
+    }
+
+    public void setPerfilXUsuarioList(List<PerfilXUsuario> perfilXUsuarioList) {
+        this.perfilXUsuarioList = perfilXUsuarioList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idUsuario != null ? idUsuario.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Usuario)) {
+            return false;
+        }
+        Usuario other = (Usuario) object;
+        if ((this.idUsuario == null && other.idUsuario != null) || (this.idUsuario != null && !this.idUsuario.equals(other.idUsuario))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ec.ruiztello.entidades.Usuario[ idUsuario=" + idUsuario + " ]";
+    }
+    
+}
